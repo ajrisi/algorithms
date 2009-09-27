43b3a95844960d2b2ce1013acfca21d9a85156d9
Adam Risi <ajrisi@gmail.com>
2009-09-27 00:12:29 -0400

    adding a simple header file that includes all of the other header files for the library

---
+++ algo.h
@@ -0,0 +1,19 @@
+/**
+ * @file   algo.h
+ * @author Adam Risi <ajrisi@gmail.com>
+ * @date   Sun Sep 27 00:08:06 2009
+ * 
+ * @brief This is the main header file for my libalgo, it just
+ * includes all of the other header files.
+ * 
+ * 
+ */
+
+
+#ifndef ALGO_H_
+#define ALGO_H_
+
+#include "minheap.h"
+#include "prng.h"
+
+#endif
